12a9cb26437360ed616f3c3d9aab4e8efad15946
Prajwal Singh <prajwal@scriptgurudigitalsolutions.com>
2025-01-13 22:34:11 +0530

    Feat:fixed ui bugs

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import Logo from "../public/BORCELLE.png";
 import { useEffect, useState } from "react";
 import { IoMenu } from "react-icons/io5";
+import { IoClose } from "react-icons/io5";
 
 export default function Navbar() {
   const [navbarChange, setNavbarChange] = useState(false);
@@ -41,36 +42,57 @@ export default function Navbar() {
             alt="logo"
             className="rounded-md"
           />
-          <span
-            onClick={() => setOpen(!open)}
-            className="cursor-pointer flex md:hidden text-2xl z-20"
-          >
-            <IoMenu />
-          </span>
+          {open ? (
+            <span
+              onClick={() => setOpen(!open)}
+              className="cursor-pointer flex md:hidden text-2xl z-20"
+            >
+              <IoClose />
+            </span>
+          ) : (
+            <span
+              onClick={() => setOpen(!open)}
+              className="cursor-pointer flex md:hidden text-2xl z-20"
+            >
+              <IoMenu />
+            </span>
+          )}
           <ul
-            className={`flex flex-col md:flex-row md:relative fixed inset-x-0 z-0 left-0 gap-6 text-lg ${
+            className={`flex flex-col md:flex-row md:relative fixed inset-x-0 z-0 py-4 left-0 gap-6 text-lg ${
               navbarChange
                 ? "bg-[#1C1678] md:bg-transparent"
                 : "bg-black md:bg-transparent"
             } text-white  ${
               open
-                ? " translate-y-40 animate-in slide-in-from-top-40"
-                : "-translate-y-40 md:translate-y-0"
+                ? " translate-y-44 animate-in slide-in-from-top-40"
+                : "-translate-y-44 md:translate-y-0"
             } `}
           >
             <li className="p-2 hover:border-b cursor-pointer">
-              <Link href={"/"}>HOME</Link>
+              <Link href={"/"} onClick={() => setOpen(!open)}>
+                HOME
+              </Link>
             </li>
             <li className="p-2 hover:border-b  cursor-pointer">
-              <Link className="mb-4" href={"#about"}>
+              <Link
+                className="mb-4"
+                href={"#about"}
+                onClick={() => setOpen(!open)}
+              >
                 ABOUT
               </Link>
             </li>
             <li className="p-2 hover:border-b cursor-pointer ">
-              <Link href={"#contact"}>CONTACT US</Link>
+              <Link href={"#contact"} onClick={() => setOpen(!open)}>
+                CONTACT US
+              </Link>
             </li>
             <li className="p-2 hover:border-b cursor-pointer ">
-              <Link href={"#services"} className="uppercase">
+              <Link
+                href={"#services"}
+                className="uppercase"
+                onClick={() => setOpen(!open)}
+              >
                 Services
               </Link>
             </li>
